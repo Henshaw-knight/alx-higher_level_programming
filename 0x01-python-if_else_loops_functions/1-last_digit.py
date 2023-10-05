9822f1c496acd7c726f8c9cc1beeafe591d5194d
Henshaw <josephhensh2000@gmail.com>
2023-10-05 12:15:22 -0700

    Write program that checks for the last digit of a random signed number and prints the required text

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = int(str(number)[-1])
+string = f"The Last digit of {number} is {last_digit}"
+
+if (last_digit > 5):
+    string += f" and is greater than 5"
+if (last_digit == 0):
+    string += " and is 0"
+if (last_digit < 6 and last_digit != 0):
+    string += " and is less than 6 and not 0"
+print(string)

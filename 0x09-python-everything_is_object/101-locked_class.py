53b40fb61661915ae863e0687942c69b8bab90bd
Henshaw <josephhensh2000@gmail.com>
2023-11-01 19:18:56 -0700

    Update file

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,20 +7,8 @@ class LockedClass:
     from dynamically creating new instance attributes, except if the new
     instance attribute is called first_name
     """
+    __slots__ = ["first_name"]
+
     def __init__(self):
-        """Initializes new object"""
-        self.first_name = None
-
-    @property
-    def first_name(self):
-        return self.__first_name
-
-    @first_name.setter
-    def first_name(self, value):
-        self.__first_name = value
-
-    def __setattr__(self, name, value):
-        if name != "first_name":
-            raise AttributeError(
-                    "'LockedClass' object has no attribute '{}'".format(name)
-            )
+        """Initializes object"""
+        pass

dbcd8026456920938aaf62b21403d7ca48dd2313
Henshaw <josephhensh2000@gmail.com>
2023-12-04 02:20:09 -0800

    Update file: add update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -42,3 +42,36 @@ class Square(Rectangle):
 
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute
+        or assigns a key/value argument to attributes
+
+        Args:
+            args (list/tuple): for any number of positional arguments
+            kwargs (dict): for any number of keyword arguments
+        """
+        if args is not None and len(args) > 0:
+            for index, arg in enumerate(args):
+                if index == 0:
+                    self.id = arg
+                elif index == 1:
+                    self.width = arg
+                    self.height = arg
+                elif index == 2:
+                    self.x = arg
+                elif index == 3:
+                    self.y = arg
+
+        else:
+            if kwargs is not None and len(kwargs) > 0:
+                for key, value in kwargs.items():
+                    if key == "id":
+                        self.id = value
+                    elif key == "size":
+                        self.width = value
+                        self.height = value
+                    elif key == "x":
+                        self.x = value
+                    elif key == "y":
+                        self.y = value

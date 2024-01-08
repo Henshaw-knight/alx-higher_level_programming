fd869e91fb68501fbbd36af1f2c5eea714106167
Henshaw <josephhensh2000@gmail.com>
2024-01-08 15:06:18 -0800

    Correct styling

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,11 +2,11 @@
 const firstArg = parseInt(process.argv[2]);
 
 if (Number.isInteger(firstArg)) {
-	let i = 1;
-	while (i <= firstArg) {
-	  console.log('C is fun');
-	  i++;
-	}
+  let i = 1;
+  while (i <= firstArg) {
+    console.log('C is fun');
+    i++;
+  }
 } else {
   console.log('Missing number of occurrences');
 }

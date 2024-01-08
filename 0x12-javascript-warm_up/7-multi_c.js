7252d84910d7f3c70c4dc087ec667f631cc12cdb
Henshaw <josephhensh2000@gmail.com>
2024-01-08 14:37:16 -0800

    Write script that prints 'C is fun' x number of times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const firstArg = parseInt(process.argv[2]);
+
+if (Number.isInteger(firstArg)) {
+	let i = 1;
+	while (i <= firstArg) {
+	  console.log('C is fun');
+	  i++;
+	}
+} else {
+  console.log('Missing number of occurrences');
+}

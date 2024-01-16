1f877175272a1c223d50a82bde40a7ef4ea8ed22
Henshaw <josephhensh2000@gmail.com>
2024-01-16 04:56:02 -0800

    Write script that lists all states from a database in the required format

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=args[1], passwd=args[2], database=args[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    results = cur.fetchall()
+
+    for row in results:
+        print(row)
+
+    cur.close()
+    db.close()

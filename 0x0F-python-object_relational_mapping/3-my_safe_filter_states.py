1a660de489b450af1ea9f0b0a4a1e067b9549a5b
Henshaw <josephhensh2000@gmail.com>
2024-01-18 02:32:49 -0800

    Update file

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,14 +8,12 @@ import sys
 if __name__ == "__main__":
     args = sys.argv
 
-    safer_state_arg = (args[4].split(';'))[0]
-
     db = MySQLdb.connect(host="localhost", port=3306, user=args[1],
                          passwd=args[2], database=args[3])
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY '{}'
-                ORDER BY states.id ASC""".format(safer_state_arg))
+    cur.execute("""SELECT * FROM states WHERE name = %s
+            ORDER BY id ASC""", (sys.argv[4],))
     results = cur.fetchall()
 
     for row in results:

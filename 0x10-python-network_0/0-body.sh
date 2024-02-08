3a8cea52b95c7942266fbd17bfb78c7eb16ff848
Henshaw <josephhensh2000@gmail.com>
2024-02-08 05:49:31 -0800

    Write Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of response

---
+++ 0x10-python-network_0/0-body.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Bash script that takes in a URL, sends a GET request to the URL,
+# and displays the body of the response
+size=$(curl -X GET -s -o /dev/null -w "%{size_download}" "$1")
+echo "$size"

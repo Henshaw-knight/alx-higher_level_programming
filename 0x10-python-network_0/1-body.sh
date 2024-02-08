59404b070e35380f46262e14465b62d488c10e27
Henshaw <josephhensh2000@gmail.com>
2024-02-08 07:24:23 -0800

    Update file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a GET request to the URL and displays the body of the response if it is a 200 status code response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -sL "$1"

6972e5382d0795f1c2b5bda16830e9c76f6bd77b
Henshaw <josephhensh2000@gmail.com>
2024-02-08 05:52:48 -0800

    Update file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL,
 # and displays the body of the response
 size=$(curl -X GET -s -o /dev/null -w "%{size_download}" "$1")

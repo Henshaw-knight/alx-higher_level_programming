60c271a6c536fcf0fff200854d3d89fe39438699
Henshaw <josephhensh2000@gmail.com>
2024-02-09 05:10:07 -0800

    Update file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containg You got me!, in the response body
-curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a  "You got me!" message in the response body
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
